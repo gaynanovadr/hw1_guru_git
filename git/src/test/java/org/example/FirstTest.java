93466179b30cdd4af69d21e8bd282f31b9c3e706
Nour Koronful <gaynanovadr@me.com>
2022-09-16 20:46:17 +0200

    first commit consol

---
+++ git/src/test/java/org/example/FirstTest.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import java.util.Random;
+
+public class FirstTest {
+
+    @Test
+    void exampleTest0() {
+        Assertions.assertFalse(2 <= 1);
+    }
+
+    @Test
+    void exampleTest1() {
+        Assertions.assertTrue(3 >= 2);
+    }
+}
